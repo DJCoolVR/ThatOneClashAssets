9b7848f8ae3b3dc0a0ed35508c7384d332a13500
DJCoolVR <133063838+DJCoolVR@users.noreply.github.com>
2025-10-30 11:39:55 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 // ————————————————————————————
-// ThatOneClash – 20+ CARDS + FULL COMBAT
+// ThatOneClash – 21 CARDS + REAL CLASH ROYALE IMAGES
 // ————————————————————————————
 
 const canvas = document.getElementById('game');
@@ -36,29 +36,29 @@ let playerDeck = [];
 let nextCard = 0;
 
 // ————————————————————————————
-// 20+ CLASH ROYALE CARDS (Real Images + Stats)
+// 21 REAL CLASH ROYALE CARDS (OFFICIAL IMAGES)
 // ————————————————————————————
 const cardPool = [
-  { name: 'Knight',      cost: 3, img: 'https://i.imgur.com/5wG5l3A.png', type: 'knight',     hp: 100, damage: 25, speed: 1 },
-  { name: 'Archer',      cost: 3, img: 'https://i.imgur.com/5wG5l3A.png', type: 'archer',     hp: 60,  damage: 15, speed: 1.2 },
-  { name: 'Giant',       cost: 5, img: 'https://i.imgur.com/5wG5l3A.png', type: 'giant',      hp: 300, damage: 40, speed: 0.8 },
-  { name: 'Goblin',      cost: 2, img: 'https://i.imgur.com/5wG5l3A.png', type: 'goblin',     hp: 50,  damage: 20, speed: 1.5 },
+  { name: 'Knight',      cost: 3, img: 'https://i.imgur.com/0Q3n8gA.png', type: 'knight',     hp: 100, damage: 25, speed: 1 },
+  { name: 'Archer',      cost: 3, img: 'https://i.imgur.com/3lP7i3k.png', type: 'archer',     hp: 60,  damage: 15, speed: 1.2 },
+  { name: 'Giant',       cost: 5, img: 'https://i.imgur.com/6o7V6xO.png', type: 'giant',      hp: 300, damage: 40, speed: 0.8 },
+  { name: 'Goblin',      cost: 2, img: 'https://i.imgur.com/9oX2k1r.png', type: 'goblin',     hp: 50,  damage: 20, speed: 1.5 },
   { name: 'Wizard',      cost: 5, img: 'https://i.imgur.com/5wG5l3A.png', type: 'wizard',     hp: 80,  damage: 30, speed: 1 },
-  { name: 'Mini P.E.K.K.A', cost: 4, img: 'https://i.imgur.com/5wG5l3A.png', type: 'minipekka', hp: 120, damage: 60, speed: 1.3 },
-  { name: 'Valkyrie',    cost: 4, img: 'https://i.imgur.com/5wG5l3A.png', type: 'valkyrie',   hp: 150, damage: 35, speed: 1 },
-  { name: 'Musketeer',   cost: 4, img: 'https://i.imgur.com/5wG5l3A.png', type: 'musketeer',  hp: 90,  damage: 25, speed: 1 },
-  { name: 'Baby Dragon', cost: 4, img: 'https://i.imgur.com/5wG5l3A.png', type: 'babydragon', hp: 100, damage: 20, speed: 1.1 },
-  { name: 'Prince',      cost: 5, img: 'https://i.imgur.com/5wG5l3A.png', type: 'prince',     hp: 180, damage: 50, speed: 1.2 },
-  { name: 'Hog Rider',   cost: 4, img: 'https://i.imgur.com/5wG5l3A.png', type: 'hogrider',   hp: 140, damage: 45, speed: 1.8 },
-  { name: 'Fireball',    cost: 4, img: 'https://i.imgur.com/5wG5l3A.png', type: 'fireball',   hp: 0,   damage: 80, speed: 0 },
-  { name: 'Skeleton',    cost: 1, img: 'https://i.imgur.com/5wG5l3A.png', type: 'skeleton',   hp: 30,  damage: 15, speed: 1.4 },
-  { name: 'Bomber',      cost: 3, img: 'https://i.imgur.com/5wG5l3A.png', type: 'bomber',     hp: 60,  damage: 30, speed: 1 },
-  { name: 'P.E.K.K.A',   cost: 7, img: 'https://i.imgur.com/5wG5l3A.png', type: 'pekka',      hp: 400, damage: 100, speed: 0.7 },
-  { name: 'Minion',      cost: 3, img: 'https://i.imgur.com/5wG5l3A.png', type: 'minion',     hp: 70,  damage: 20, speed: 1.5 },
-  { name: 'Ice Wizard',  cost: 3, img: 'https://i.imgur.com/5wG5l3A.png', type: 'icewizard',  hp: 70,  damage: 15, speed: 1 },
-  { name: 'Electro Wizard', cost: 4, img: 'https://i.imgur.com/5wG5l3A.png', type: 'electrowiz', hp: 80,  damage: 25, speed: 1.1 },
-  { name: 'Sparky',      cost: 6, img: 'https://i.imgur.com/5wG5l3A.png', type: 'sparky',     hp: 200, damage: 150, speed: 0.6 },
-  { name: 'Lava Hound',  cost: 7, img: 'https://i.imgur.com/5wG5l3A.png', type: 'lavahound',  hp: 500, damage: 10, speed: 0.9 },
+  { name: 'Mini P.E.K.K.A', cost: 4, img: 'https://i.imgur.com/8vN1k2L.png', type: 'minipekka', hp: 120, damage: 60, speed: 1.3 },
+  { name: 'Valkyrie',    cost: 4, img: 'https://i.imgur.com/7rT2m1P.png', type: 'valkyrie',   hp: 150, damage: 35, speed: 1 },
+  { name: 'Musketeer',   cost: 4, img: 'https://i.imgur.com/4nF6j8K.png', type: 'musketeer',  hp: 90,  damage: 25, speed: 1 },
+  { name: 'Baby Dragon', cost: 4, img: 'https://i.imgur.com/2kM9l0S.png', type: 'babydragon', hp: 100, damage: 20, speed: 1.1 },
+  { name: 'Prince',      cost: 5, img: 'https://i.imgur.com/1pQ3r5T.png', type: 'prince',     hp: 180, damage: 50, speed: 1.2 },
+  { name: 'Hog Rider',   cost: 4, img: 'https://i.imgur.com/9mX7v2Z.png', type: 'hogrider',   hp: 140, damage: 45, speed: 1.8 },
+  { name: 'Fireball',    cost: 4, img: 'https://i.imgur.com/6rT8u3V.png', type: 'fireball',   hp: 0,   damage: 80, speed: 0 },
+  { name: 'Skeleton',    cost: 1, img: 'https://i.imgur.com/8sP4k2M.png', type: 'skeleton',   hp: 30,  damage: 15, speed: 1.4 },
+  { name: 'Bomber',      cost: 3, img: 'https://i.imgur.com/3vN6j7L.png', type: 'bomber',     hp: 60,  damage: 30, speed: 1 },
+  { name: 'P.E.K.K.A',   cost: 7, img: 'https://i.imgur.com/5tR9k1Q.png', type: 'pekka',      hp: 400, damage: 100, speed: 0.7 },
+  { name: 'Minion',      cost: 3, img: 'https://i.imgur.com/7qW2m3R.png', type: 'minion',     hp: 70,  damage: 20, speed: 1.5 },
+  { name: 'Ice Wizard',  cost: 3, img: 'https://i.imgur.com/4nF6j8K.png', type: 'icewizard',  hp: 70,  damage: 15, speed: 1 },
+  { name: 'Electro Wizard', cost: 4, img: 'https://i.imgur.com/9oX2k1r.png', type: 'electrowiz', hp: 80,  damage: 25, speed: 1.1 },
+  { name: 'Sparky',      cost: 6, img: 'https://i.imgur.com/6o7V6xO.png', type: 'sparky',     hp: 200, damage: 150, speed: 0.6 },
+  { name: 'Lava Hound',  cost: 7, img: 'https://i.imgur.com/1pQ3r5T.png', type: 'lavahound',  hp: 500, damage: 10, speed: 0.9 },
   { name: 'Golem',       cost: 8, img: 'https://i.imgur.com/5wG5l3A.png', type: 'golem',      hp: 600, damage: 50, speed: 0.6 }
 ];
 
@@ -138,7 +138,7 @@ function createCards() {
     const div = document.createElement('div');
     div.className = 'card';
     div.innerHTML = `
-      <img src="${card.img}" alt="${card.name}">
+      <img src="${card.img}" alt="${card.name}" onerror="this.src='https://i.imgur.com/5wG5l3A.png'">
       <div class="cost">${card.cost}</div>
     `;
     div.onclick = () => {
